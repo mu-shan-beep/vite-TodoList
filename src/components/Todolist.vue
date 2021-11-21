572e54159051ee2319d0df91c5024a470fb7ee0c
木杉 <2840891093@qq.com>
2021-11-21 15:05:14 +0800

    初始配置router

---
+++ src/components/Todolist.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 @click="add">{{ count }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+defineProps({});//父组件传值
+
+let count = ref(1);
+function add() {
+  count.value++;
+
+}
+
+
+</script>
+
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,3 @@
+<template>
+	<h1>我是Home页面</h1>
+</template>
